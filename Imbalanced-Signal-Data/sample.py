2153128f7a4483d371c1a4a06921fde46ace7ec5
Dongjin Seo <65718383+dongjin-seo2020@users.noreply.github.com>
2021-05-28 15:13:21 +0900

    Update sample.py

--- Imbalanced-Signal-Data/sample.py
+++ Imbalanced-Signal-Data/sample.py
@@ -5,6 +5,7 @@ import os
 from utils import *
 
 ### data import
-
+traindata = pd.read_csv('mitbih_train.csv', header=None)
+testdata = pd.read_csv('mitbih_test.csv',header=None)
 
 ### write baseline code for newbies
